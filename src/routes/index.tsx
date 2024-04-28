c19c087d84b05e78cd11be84f9491a154e8c2fd9
henri <henri.me@skiff.com>
2024-04-28 12:09:03 +0200

    add tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,23 +3,18 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <div>
+      
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "henri",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Hey, my name is henri, I'm 14 years old and a mostly active in the frontend space. I like to build things like my own email provider.",
     },
   ],
 };
