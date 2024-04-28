f41e108c76ca824f1b729c411f7707cae101d32e
henri <henri.me@skiff.com>
2024-04-28 21:25:09 +0200

    change the breakpoint

--- src/routes/[...lang]/index.tsx
+++ src/routes/[...lang]/index.tsx
@@ -27,7 +27,7 @@ export default component$(() => {
           <Link class="text-2xl animate-bottom_fade" href='/contact'>Contact</Link>
         </div>
       </div>
-      <div class="xl:w-[700px] flex flex-col space-y-16">
+      <div class="lg:w-[700px] flex flex-col space-y-16">
         <div class="flex items-center justify-between mt-16">
           <div class="flex items-center justify-center  space-x-3">
             <img onClick$={() => nav("#contact")} class="w-[40px] h-[40px] relative top-1 hover:ring-[#ffffff] cursor-pointer hover:dark:ring-[#282828] ring-2 ring-[#ffffffc0] dark:ring-[#282828c0]  ring-offset-2 hover:ring-offset-3 transition-all ring-offset-[#e1e1e1]  dark:ring-offset-[#171717] rounded-full" src="https://avatars.githubusercontent.com/u/98414850?v=4" alt="avatar" width={"40"} height={"40"} />
