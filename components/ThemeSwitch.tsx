cc58ecb1f57382d511c369293e91ae9352a01bf2
henri <henri.me@skiff.com>
2024-04-30 20:04:56 +0200

    redesign the switch

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MoonStars, Sun } from '@phosphor-icons/react';
+import { Moon, Sun } from '@phosphor-icons/react';
 import { motion } from 'framer-motion';
 import { useTheme } from 'next-themes';
 
@@ -15,39 +15,17 @@ export default function ThemeSwitch() {
     ready: Promise<void>;
     updateCallbackDone: Promise<void>;
   }
-  
-  interface GlobalDocument extends Document  {
+
+  interface GlobalDocument extends Document {
     startViewTransition?(setupPromise: () => Promise<void> | void): ViewTransition;
   }
   const glbDocument: GlobalDocument = document
   return (
-    <motion.button
-      className='flex size-11 items-center justify-center overflow-hidden rounded-full border bg-[linear-gradient(320deg,#f1f1f1,#ffffff)] shadow-lg dark:bg-[linear-gradient(320deg,#000000,#5a5752)] sm:size-12'
-      onClick={() => {
-        if (!glbDocument.startViewTransition) switchTheme();
-        glbDocument!.startViewTransition!(switchTheme);
-      }}
-      whileHover={{
-        scale: 1.1,
-      }}
-    >
-      <div className='flex h-full w-full items-center justify-center overflow-hidden rounded-full shadow-inner'>
-        <motion.div
-          className='flex items-center gap-12'
-          animate={{
-            x: resolvedTheme === 'dark' ? 36 : -36,
-            transition: {
-              type: 'spring',
-              duration: 1.2,
-              bounce: 0.6,
-            },
-          }}
-        >
-          <MoonStars className='size-6' />
-          <Sun className='size-6' />
-        </motion.div>
-        <span className='sr-only'>Toggle theme</span>
-      </div>
-    </motion.button>
+    <div onClick={() => {
+      if (!glbDocument.startViewTransition) switchTheme();
+      glbDocument!.startViewTransition!(switchTheme);
+    }} className='rounded-full border p-2 cursor-pointer'>
+      {resolvedTheme == "dark" ? <Moon /> : <Sun />}
+    </div>
   );
 }
