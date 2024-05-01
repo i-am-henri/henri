81b8e25442dcf3abcbfa6c1b8aa5e0ff1bf6bee1
henri <henri.me@skiff.com>
2024-05-01 21:00:09 +0200

    add support for smal devices

--- app/[...lang]/page.tsx
+++ app/[...lang]/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
         <div className="min-h-screen flex flex-col items-center bg-[#e1e1e1] dark:bg-[#171717]">
 
             <Navigation locale={t("locale")} land={t("app.work.land")} work={t("app.work.title")} />
-            <div className="lg:w-[700px] flex flex-col space-y-16 mt-16">
+            <div className="w-full p-5 sm:p-0 sm:w-[400px] md:w-[500px] lg:w-[700px] flex flex-col space-y-16 mt-16">
                 <div>
                     <h2>{t("app.history.title")}</h2>
                     <p>{t("app.history.content")}</p>
