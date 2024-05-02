5b63d28fc08e1a075c0e0b55c0b503957c4736ed
henri <henri.me@skiff.com>
2024-05-02 18:03:05 +0200

    add better lightmode support

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -56,18 +56,17 @@ export default function Home() {
                     <h2>Skills</h2>
                     <p>I have 3 years of experience in the webdevelopment space.</p>
                 </div>
-                <hr className="border-t dark:border-t-[#343434] w-full" />
+                <hr className="border-t border-t-[#c1c1c1] dark:border-t-[#343434] w-full" />
 
-                <footer className="dark:text-[#343434] flex justify-between">
+                <footer className="text-[#c1c1c1] dark:text-[#343434] flex justify-between">
                     <div>
-                        <p className="hover:dark:text-[#ffffffc0] transition duration-300">handmade by henri</p>
+                        <p className="text-[#c1c1c1] dark:text-[#343434] hover:dark:text-[#ffffffc0] transition duration-300">handmade by henri</p>
                     </div>
                     <div className="flex">
-                        <Link href={"https://github.com/i-am-henri"} className="border dark:border-[#343434] p-2 transition duration-300 hover:dark:text-[#ffffffc0] hover:dark:border-[#ffffffc0] rounded-full h-min" target="_blank"><GithubLogo /></Link>
+                        <Link href={"https://github.com/i-am-henri"} className="border border-[#c1c1c1] dark:border-[#343434] p-2 transition duration-300 hover:dark:text-[#ffffffc0] hover:dark:border-[#ffffffc0] rounded-full h-min" target="_blank"><GithubLogo  /></Link>
                     </div>
                 </footer>
             </div>
-            <button className="group relative inline-flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-neutral-950 font-medium text-neutral-200 transition-all duration-300 hover:w-32"><div className="inline-flex whitespace-nowrap opacity-0 transition-all duration-200 group-hover:-translate-x-3 group-hover:opacity-100">Hover me</div><div className="absolute right-3.5"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></button>
         </div>
     )
 }
