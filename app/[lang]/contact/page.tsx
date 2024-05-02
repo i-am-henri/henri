338001897df5107aabb82547b1cb4a6254d7c3b6
henri <henri.me@skiff.com>
2024-05-02 19:57:26 +0200

    add responsebility for the grid

--- app/[lang]/contact/page.tsx
+++ app/[lang]/contact/page.tsx
@@ -13,14 +13,14 @@ export default function Work() {
                     <h2>{t("contact.title")}</h2>
                     <p>{t("contact.content")}</p>
                 </div>
-                <div className="grid grid-cols-3">
-                    <Link className="flex items-center" href={"https://git.new/henri"}>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+                    <Link className="flex items-center" href={"https://git.new/henri"} target="_blank">
                         <GithubLogo className="mr-2"/> i-am-henri
                     </Link>
-                    <Link className="flex items-center" href={"https://git.new/henri"}>
-                        <DiscordLogo className="mr-2"/> henri.web
+                    <Link className="flex items-center" href={"discordapp.com/users/830760615789854742"} target="_blank">
+                        <DiscordLogo className="mr-2"/> @henri.web
                     </Link>
-                    <Link className="flex items-center" href={"mailto:me@henri.gg"}>
+                    <Link className="flex items-center" href={"mailto:me@henri.gg"} target="_blank">
                         <Mailbox className="mr-2"/> me@henri.gg
                     </Link>
                 </div>
