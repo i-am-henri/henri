f57d934a620d68d720201d8584eed12222724f30
henri <henri.me@skiff.com>
2024-05-02 18:31:12 +0200

    add a component lab

--- app/[lang]/lab/page.tsx
+++ app/[lang]/lab/page.tsx
@@ -8,10 +8,11 @@ export default function Work() {
         <div className="min-h-screen flex flex-col items-center bg-[#e1e1e1] dark:bg-[#171717]">
             <Navigation locale={t("locale")} land={t("app.work.land")} work={t("app.work.title")} />
             <div className="w-full p-5 sm:p-0 sm:w-[400px] md:w-[500px] lg:w-[700px] flex flex-col space-y-16 mt-16">
-                <Link href={t("work.cookie.link")} target="_blank">
-                    <h2>{t("work.cookie.title")}</h2>
-                    <p>{t("work.cookie.content")}</p>
-                </Link>
+                <div>
+                    <h2>{t("lab.title")}</h2>
+                    <p>{t("lab.description")}</p>
+                </div>
+                <p className="text-[#c1c1c1] dark:text-[#343434]">nothing here... for now!</p>
 
             </div>
         </div>
