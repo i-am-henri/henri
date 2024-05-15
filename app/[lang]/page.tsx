6b1f9370d7c25243d04bc865a81f9aaec521a326
Henri <98414850+i-am-henri@users.noreply.github.com>
2024-05-15 12:49:51 +0000

    add changing effect

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
     const t = useTranslations()
     return (
         <div className="min-h-screen flex flex-col items-center bg-[#e1e1e1] dark:bg-[#171717]">
-            <Navigation locale={t("locale")} land={t("app.work.land")} work={t("app.work.title")} />
+            <Navigation locale={t("locale")} land={t("app.work.land")} work={[t("app.work.title"), t("app.work.secondtitle"), t("app.work.thirdtitle"), t("app.work.fourttitle")]} />
             <div className="w-full p-5 sm:p-0 sm:w-[400px] md:w-[500px] lg:w-[700px] flex flex-col space-y-16 mt-16">
                 <div>
                     <h2>{t("app.history.title")}</h2>
