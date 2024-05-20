12da59571016ff99d54ef1323d4e0f3de81b2def
henri <henri.me@skiff.com>
2024-05-20 16:58:04 +0200

    removed 404 page

--- app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-    return (
-        <div className="min-h-screen flex w-full items-center justify-center">
-            <h2>not found :/</h2>
-        </div>
-    )
-}
