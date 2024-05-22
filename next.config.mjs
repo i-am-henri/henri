f1b6bad8eaa7de56d66b278f0a57e7e2a76da21e
henri <henri.me@skiff.com>
2024-05-22 13:57:40 +0200

    add umami rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,16 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/umami",
+                destination: "https://cloud.umami.is/script.js",
+            },
+        ];
+    },
     distDir: "out"
 };
 
