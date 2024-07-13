7ba4760ccc889d82c857aaae8fcc711e4974bbcf
henri <henri.me@skiff.com>
2024-07-13 10:51:36 +0200

    add a better project card

--- app/[lang]/work/page.tsx
+++ app/[lang]/work/page.tsx
@@ -1,14 +1,15 @@
 import { getAllWorkPosts } from "./work"
 import Img from "next/image"
-import contentio from "../../../content/project/contentio.png"
+
 export default async function Work() {
     const posts = await getAllWorkPosts()
     return (
-        <div>
+        <div className="grid grid-cols-2 gap-4">
             {posts.length !== 0 && posts.map((post) => (
                 <div key={post.title}>
-                    <Img className="rounded-md border" src={post.image} alt="This is a image" width={400} height={400} />
-                    {post.title}
+                    <Img className="rounded-md border" src={post.image} alt="This is a image"  width={"1000"} height={"1000"}/>
+                    <h2 className="text-lg m-0">{post.title}</h2>
+                    <p>{post.description}</p>
                 </div>
             )) || (
                 <p>You have no content yet.</p>
