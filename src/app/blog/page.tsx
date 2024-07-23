7fb401fe6f1083356ea504c5451c68cb7c6e1a4f
henri <henri.me@skiff.com>
2024-07-23 17:31:44 +0200

    wasn't able to fix the issue with the type prediction

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,4 @@
-import { z, ZodObject, ZodSchema, ZodString } from "zod"
+import { Schema, z, ZodObject, ZodSchema, ZodString } from "zod"
 import fs from "node:fs/promises"
 import matter from "gray-matter"
 import { title } from "node:process"
@@ -8,16 +8,16 @@ export const runtime = "nodejs"
 export default async function Blog() {
     const posts = await fetchAllPosts({
         folder: "./src/posts/",
-        schema: z.object({
+        schema: {
             title: z.string()
-        })
+        }
     })
     return (
         <div className="space-y-12 flex flex-col lg:w-[500px]">
             {
                 posts.map((post) => (
-                    <div key={post.frontmatter.toString()}>
-                        {post.content}
+                    <div key={post.frontmatter.title}>
+                        {post.frontmatter.title}
                     </div>
                 ))
             }
@@ -27,20 +27,28 @@ export default async function Blog() {
 export async function fetchAllPosts({
     schema,
     folder
-}:{
-    schema: ZodSchema<object>
+}: {
+    schema: {
+        [key: string]: ZodSchema<any>;
+    }
     folder: string
 }) {
     "use server"
-    let arr: {
-      frontmatter: {},
-      content: string,
-      meta: {
-        file: string,
-        slug: string,
-        folder: string
-      }
+    type InferSchema<T extends { [key: string]: ZodSchema<any> }> = {
+        [K in keyof T]: z.infer<T[K]>;
+      };
+    const parseSchema = z.object({
+        ...schema
+    })
 
+    let arr: {
+        frontmatter: InferSchema<typeof schema>,
+        content: string,
+        meta: {
+            file: string,
+            slug: string,
+            folder: string
+        }
     }[] = []
 
     const dir = await fs.readdir(folder)
@@ -50,9 +58,9 @@ export async function fetchAllPosts({
         const data = matter(content)
 
         // parsing the frontmatter
-        const parse = await schema.safeParseAsync({...data.data})
+        const parse = await parseSchema.safeParseAsync({ ...data.data })
         if (!parse.success) throw new Error(`Wrong frontmatter in file ${file} of the folder "${folder}`)
-     
+
         arr.push({
             frontmatter: {
                 ...data.data
