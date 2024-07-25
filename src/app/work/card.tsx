fe67caf78de48f84815f5b088330b4c328ebe6e2
henri <henri.me@skiff.com>
2024-07-25 11:03:14 +0200

    implemented mobile drawer

--- src/app/work/card.tsx
+++ src/app/work/card.tsx
@@ -1,4 +1,5 @@
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTrigger } from "@/components/ui/dialog"
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTrigger } from "@/components/ui/drawer"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -22,7 +23,7 @@ export default function WorkCard({
     return (
         <>
             <Dialog>
-                <DialogTrigger className="flex flex-col hover:bg-popover duration-200 rounded-md border p-2 justify-start">
+                <DialogTrigger className="hidden md:flex flex-col hover:bg-popover duration-200 rounded-md border p-2 justify-start">
                     <h3 className="text-lg font-medium">
                         {title}
                     </h3>
@@ -57,6 +58,42 @@ export default function WorkCard({
                     </DialogFooter>
                 </DialogContent>
             </Dialog>
+            <Drawer>
+                <DrawerTrigger className="flex md:hidden flex-col hover:bg-popover duration-200 rounded-md border p-2 justify-start">
+                    <h3 className="text-lg font-medium">
+                        {title}
+                    </h3>
+                    {image && (
+                        <Image src={image} alt={description} />
+                    )}
+                    <p className="text-start">
+                        {description}
+                    </p>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        {title}
+                    </DrawerHeader>
+                    <DrawerDescription>
+                        {content}
+                    </DrawerDescription>
+                    <DrawerFooter className="flex items-center space-x-2">
+                        {github && (
+                            <Link href={github}>
+                                GitHub
+                            </Link>
+                        )}
+                        {link && (
+                            <Link href={link}>
+                                Visit
+                            </Link>
+                        )}
+                        <DrawerClose>
+                            Ok
+                        </DrawerClose>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
         </>
     )
 }
